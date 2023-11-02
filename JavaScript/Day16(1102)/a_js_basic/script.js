9666023901a2a3ad9e75688d65636c6059f86623
i12free011@gmail.com <i12free011@gmail.com>
2023-11-02 17:59:21 +0900

    231102

---
+++ JavaScript/Day16(1102)/a_js_basic/script.js
@@ -0,0 +1,4 @@
+// document.write("<h1>JS 출력</h1>");
+
+let userName = prompt("이름을 입력하세요.");
+document.write(`<h1>Hi, ${userName} !</h1>`);
---
+++ JavaScript/Day16(1102)/z_review/script.js
@@ -0,0 +1,13 @@
+// 문제 1
+let r = 10;
+const PI = 3.14;
+
+let area = r * r * PI;
+console.log(`넓이 : ${area}`);
+
+
+// 문제 2
+let inch = (prompt("inch 값을 입력해주세요."));
+let cm = inch * 2.54;
+
+alert(`${inch}inch 는 ${cm}cm 입니다.`);
